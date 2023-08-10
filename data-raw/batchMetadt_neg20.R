ecd3da0d5c6b254c190384cdaf04c876ee494bb6
Cristian Quiroz-Moreno <cristianquirozd1997@gmail.com>
2023-08-09 23:24:21 -0400

    Updating Metabolite metadata

--- data-raw/batchMetadt_neg20.R
+++ data-raw/batchMetadt_neg20.R
@@ -10,7 +10,8 @@ library_info <- dplyr::left_join(batchRead_Neg20, lb_metadata)
 batchMetadt_neg20 <- library_info %>%
   dplyr::mutate(PRECURSORTYPE = "[M-H]-",
                 COLLISIONENERGY = "20eV",
-                INSTRUMENTTYPE = "QTOF Agilent 6545") %>%
+                INSTRUMENTTYPE = "QTOF Agilent 6545",
+                IONMODE= "Negative") %>%
   dplyr::select(-Ionization_mode, -min_rt, -max_rt, -File) %>%
   dplyr::rename(NAME = Name, FORMULA = Formula)
 
