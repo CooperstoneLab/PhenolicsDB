da45d6e7b71346d989948c3bb2f2589107c3fe68
Cristian Quiroz-Moreno <cristianquirozd1997@gmail.com>
2023-08-16 17:48:30 -0400

    Batch Metabolite DT for Neg 40 6545

--- R/data.R
+++ R/data.R
@@ -27,7 +27,7 @@
 #' in the final library file.
 #'
 #' This specific data frame contains the metabolite information that was
-#' captured in negative polarity at 20 eV (CE) in the Agilent 6545.
+#' captured in negative polarity at 20 eV (CE) in the Agilent QTOF 6545.
 #'
 #' @format
 #'
@@ -67,3 +67,28 @@
 #' }
 #'
 "BatchRead_Neg20_6545"
+
+
+#' Metabolite metadata to be included in the final library file
+#'
+#' This data frame contains the metabolite metadata to be included
+#' in the final library file.
+#'
+#' This specific data frame contains the metabolite information that was
+#' captured in negative polarity at 40 eV (CE) in the Agilent QTOF 6545.
+#'
+#' @format
+#'
+#' \describe{
+#'   \item{NAME}{character, metabolite name}
+#'   \item{FORMULA}{character, chemical formula}
+#'   \item{INCHIKEY}{character, inchikey recovered form PUBCHEM}
+#'   \item{SMILES}{character, canonical smiles recovered drom PUBCHEM}
+#'   \item{PRECURSORTYPE}{character, precursor type `[M+H]+` for postive and `[M-H]-` for negative polarity}
+#'   \item{COLLISIONENERGY}{character, collision energy used for MS/MS}
+#'   \item{INSTRUMENTTYPE}{character, intrument type used for data collection}
+#'   \item{IONMODE}{character, `Positive` or `Negative`, restricted characters}
+#' }
+#'
+"BatchMetaDT_Neg40_6545"
+
---
+++ data-raw/BatchMetDT_Neg40_6545.R
@@ -0,0 +1,3 @@
+## code to prepare `BatchMetDT_Neg40_6545` dataset goes here
+
+usethis::use_data(BatchMetDT_Neg40_6545, overwrite = TRUE)
