fdabbc20cfb29a55a3382b7e7e963b90d65724a2
Cristian Quiroz-Moreno <cristianquirozd1997@gmail.com>
2023-08-16 16:58:47 -0400

    updating 40 Neg 6545

---
+++ data-raw/batchRead_Neg40_6545.R
@@ -0,0 +1,18 @@
+## code to prepare `batchRead_Neg40_6546` dataset goes here
+
+batch_read20_6545 <- read.csv("inst/extdata/batch_read_neg20.csv")
+
+batch_read20_6545 %>%
+  mutate(File = str_replace(string = .$File,
+                            pattern = "20eV", replace = "40eV") ) %>%
+  mutate(Filepath = paste0("inst/extdata/QTOF_6545/Neg/40/", File) ) %>%
+  mutate(FileExis = file.exists(.$Filepath))
+
+# Export sheet "40 neg" from batch_readneg20.xlsx
+batch_read_neg40_6545 <- read.csv("inst/extdata/batch_read_neg40_6545.csv") %>%
+  filter(!(File %in% "") )
+
+write.csv(batch_read_neg40_6545, file = "inst/extdata/batch_read_neg40.csv",row.names = F)
+
+
+usethis::use_data(batchRead_Neg40_6546, overwrite = TRUE)
