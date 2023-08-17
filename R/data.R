4057e0337caa74ae0da229326c9958753787993f
Cristian Quiroz-Moreno <cristianquirozd1997@gmail.com>
2023-08-16 23:49:54 -0400

    adding read and metadata for 40 POS

--- R/data.R
+++ R/data.R
@@ -54,7 +54,7 @@
 #' Metabolites included in this list were analyzed by targeted methods using
 #' an Agilent 6545 QTOF.
 #'
-#' @format read_neg20_6545
+#' @format read_neg40_6545
 #'
 #' A data frame containing 58 observations (metabolites) and 6 columns
 #' \describe{
@@ -66,7 +66,7 @@
 #'   \item{File}{character, filename not including the path}
 #' }
 #'
-"read_neg20_6545"
+"read_neg40_6545"
 
 
 #' Metabolite metadata for final library file for Negative polarity at 40 eV
@@ -139,3 +139,50 @@
 #'
 "metdt_pos20_6545"
 
+
+#' Metabolite information to batch read in Positive polarity at 40 eV CE
+#'
+#' This data frame contains the basic info required to batch import multiple
+#' files to create in-house libraries by using the
+#' [MS2extract](https://github.com/CooperstoneLab/MS2extract) package.
+#'
+#' Metabolites included in this list were analyzed by targeted methods using
+#' an Agilent 6545 QTOF.
+#'
+#' @format read_pos40_6545
+#'
+#' A data frame containing 58 observations (metabolites) and 6 columns
+#' \describe{
+#'   \item{Name}{character, metabolite name}
+#'   \item{Formula}{character, chemical formula of the metabolite}
+#'   \item{Ionizatioin_move}{character, the polarity (Positive or Negative)}
+#'   \item{min_rt}{integer, minimum retention time of metabolite elution (seconds)}
+#'   \item{max_rt}{integer, maximum retention time of metabolite elution (seconds)}
+#'   \item{File}{character, filename not including the path}
+#' }
+#'
+"read_pos40_6545"
+
+
+#' Metabolite metadata for final library file for Positive polarity at 40 eV
+#'
+#' This data frame contains the metabolite metadata to be included
+#' in the final library file.
+#'
+#' This specific data frame contains the metabolite information that was
+#' captured in negative polarity at 40 eV (CE) in the Agilent QTOF 6545.
+#'
+#' @format metdt_pos40_6545
+#'
+#' \describe{
+#'   \item{NAME}{character, metabolite name}
+#'   \item{FORMULA}{character, chemical formula}
+#'   \item{INCHIKEY}{character, inchikey recovered form PUBCHEM}
+#'   \item{SMILES}{character, canonical smiles recovered drom PUBCHEM}
+#'   \item{PRECURSORTYPE}{character, precursor type `[M+H]+` for postive and `[M-H]-` for negative polarity}
+#'   \item{COLLISIONENERGY}{character, collision energy used for MS/MS}
+#'   \item{INSTRUMENTTYPE}{character, intrument type used for data collection}
+#'   \item{IONMODE}{character, `Positive` or `Negative`, restricted characters}
+#' }
+#'
+"metdt_pos40_6545"
---
+++ data-raw/metdt_pos40_6545.R
@@ -0,0 +1,17 @@
+## code to prepare `metdt_pos40_6545` dataset goes here
+
+lb_metadata <- read.csv("inst/extdata/batch_msp_metadata.csv")
+batchRead_Pos40 <- read.csv("inst/extdata/batch_read_pos40.csv")
+
+library_info <- dplyr::left_join(batchRead_Pos40, lb_metadata)
+
+metdt_pos40_6545 <- library_info %>%
+  dplyr::mutate(PRECURSORTYPE = "[M+H]+",
+                COLLISIONENERGY = "40eV",
+                INSTRUMENTTYPE = "QTOF Agilent 6545",
+                IONMODE= "Positive") %>%
+  dplyr::select(-Ionization_mode, -min_rt, -max_rt, -File) %>%
+  dplyr::rename(NAME = Name, FORMULA = Formula)
+
+
+usethis::use_data(metdt_pos40_6545, overwrite = TRUE)
