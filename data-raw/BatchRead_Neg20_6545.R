bc9391937484cda65b9d7f699e2cea93d444e94c
Cristian Quiroz-Moreno <cristianquirozd1997@gmail.com>
2023-10-02 15:58:05 -0400

    changing quercetin3shophoroside name

--- data-raw/BatchRead_Neg20_6545.R
+++ data-raw/BatchRead_Neg20_6545.R
@@ -2,6 +2,11 @@
 
 read_neg20_6545 <-  read_excel("inst/extdata/batch_read_neg20.xlsx",
                                sheet = "20 neg") %>%
-  drop_na()
+  drop_na()  %>%
+  mutate(COLLISIONENERGY = "20 eV")
+
+ check <- read_neg20_6545 %>%
+  mutate(File = paste0("inst/extdata/QTOF_6545/Neg/20/",File)) %>%
+  mutate(FileExist = file.exists(File))
 
 usethis::use_data(read_neg20_6545, overwrite = TRUE)
