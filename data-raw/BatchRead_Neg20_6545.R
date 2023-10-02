0c103a4cb54006a1b9d5c82f712aaf294a5260c7
Cristian Quiroz-Moreno <cristianquirozd1997@gmail.com>
2023-10-02 15:28:56 -0400

    Updating neg 6545

--- data-raw/BatchRead_Neg20_6545.R
+++ data-raw/BatchRead_Neg20_6545.R
@@ -1,5 +1,7 @@
 ## code to prepare `BatchRead_Neg20_6545` dataset goes here
 
-read_neg20_6545 <- read.csv("inst/extdata/batch_read_neg20.csv")
+read_neg20_6545 <-  read_excel("inst/extdata/batch_read_neg20.xlsx",
+                               sheet = "20 neg") %>%
+  drop_na()
 
 usethis::use_data(read_neg20_6545, overwrite = TRUE)
