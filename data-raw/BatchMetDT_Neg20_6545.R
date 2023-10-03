4d47d9287ad9a0ca87aea20c0c50e48d9e40ded9
Cristian Quiroz-Moreno <cristianquirozd1997@gmail.com>
2023-10-03 10:51:39 -0400

    Updating met dt neg 6545

--- data-raw/BatchMetDT_Neg20_6545.R
+++ data-raw/BatchMetDT_Neg20_6545.R
@@ -1,18 +1,18 @@
 ## code to prepare `BatchMetDT_Neg20_6545` dataset goes here
 
+library(tidyverse)
+library(readxl)
 
+lb_metadata <- read_xlsx("inst/extdata/batch_msp_metadata.xlsx", sheet = 1)
 
-lb_metadata <- read.csv("inst/extdata/csv/batch_msp_metadata.csv") %>%
-  filter(!(SMILES %in% ""))
-batchRead_Neg20 <- read.csv("inst/extdata/csv/batch_read_neg20.csv") %>%
-  filter(!(File %in% ""))
+load("data/read_neg20_6545.rda")
 
-library_info <- dplyr::left_join(batchRead_Neg20, lb_metadata)
+library_info <- dplyr::left_join(read_neg20_6545, lb_metadata)
 
 metdt_neg20_6545 <- library_info %>%
   dplyr::mutate(PRECURSORTYPE = "[M-H]-",
                 COLLISIONENERGY = "20eV",
-                INSTRUMENTTYPE = "LC-ESI-QTOF",
+                INSTRUMENTTYPE = "ESI-QTOF",
                 IONMODE= "Negative") %>%
   dplyr::select(-Ionization_mode, -min_rt, -max_rt, -File) %>%
   dplyr::rename(NAME = Name, FORMULA = Formula)
