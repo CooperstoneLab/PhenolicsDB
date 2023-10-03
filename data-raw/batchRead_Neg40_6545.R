ec5e4027cbd9e843c24527e7cc9ecc632a292ac3
Cristian Quiroz-Moreno <cristianquirozd1997@gmail.com>
2023-10-03 13:58:34 -0400

    Updating read neg 40

--- data-raw/batchRead_Neg40_6545.R
+++ data-raw/batchRead_Neg40_6545.R
@@ -1,16 +1,14 @@
 ## code to prepare `batchRead_Neg40_6546` dataset goes here
 library(tidyverse)
 
-read_neg40_6545 <- read.csv("inst/extdata/csv/batch_read_neg40_6545.csv") %>%
-  filter(!(File %in% "") )
+read_neg40_6545 <-  read_excel("inst/extdata/batch_read_neg20.xlsx",
+                               sheet = "40 Neg") %>%
+  drop_na()  %>%
+  mutate(COLLISIONENERGY = "40 eV")
 
-read_neg40_6545 %>%
-  mutate(Filepath = paste0("inst/extdata/QTOF_6545/Neg/40/", File) ) %>%
-  mutate(FileExis = file.exists(.$Filepath))
-
-# Export sheet "40 neg" from batch_readneg20.xlsx
-
-#write.csv(read_neg40_6545, file = "inst/extdata/batch_read_neg40.csv",row.names = F)
+check <- read_neg40_6545 %>%
+  mutate(File = paste0("inst/extdata/QTOF_6545/Neg/40/",File)) %>%
+  mutate(FileExist = file.exists(File))
 
 
 usethis::use_data(read_neg40_6545, overwrite = TRUE)
