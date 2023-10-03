784b8c2f76ce2520d06d31741c3e526665141070
Cristian Quiroz-Moreno <cristianquirozd1997@gmail.com>
2023-10-03 16:15:24 -0400

    Updating naringin chem formula

--- data-raw/BatchRead_Pos20_6545.R
+++ data-raw/BatchRead_Pos20_6545.R
@@ -1,17 +1,16 @@
 ## code to prepare `BatchRead_Pos20_6545` dataset goes here
 
+library(tidyverse)
+library(readxl)
 
+read_pos20_6545 <-  read_excel("inst/extdata/batch_read_neg20.xlsx",
+                               sheet = "20 pos") %>%
+  drop_na()  %>%
+  mutate(COLLISIONENERGY = "20 eV")
 
-
-# Export sheet "20 pos" from batch_readneg20.xlsx
-read_pos20_6545 <- read.csv("inst/extdata/csv/batch_read_pos20_6545.csv") %>%
-  filter(!(File %in% "") )
-
-read_pos20_6545 %>%
-  mutate(File = str_replace(string = .$File,
-                            pattern = "_neg_", replace = "_pos_") ) %>%
-  mutate(Filepath = paste0("inst/extdata/QTOF_6545/Pos/20/", File) ) %>%
-  mutate(FileExis = file.exists(.$Filepath))
+check <- read_pos20_6545 %>%
+  mutate(File = paste0("inst/extdata/QTOF_6545/Pos/20/",File)) %>%
+  mutate(FileExist = file.exists(File))
 
 #write.csv(read_pos20_6545, file = "inst/extdata/batch_read_neg20.csv",row.names = F)
 
