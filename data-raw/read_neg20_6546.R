c75744b40b91064f5478ddcd2eed77807a4f4182
Cristian Quiroz-Moreno <cristianquirozd1997@gmail.com>
2023-10-03 23:48:41 -0400

    Update neg 20 6546

--- data-raw/read_neg20_6546.R
+++ data-raw/read_neg20_6546.R
@@ -5,6 +5,7 @@ library(readxl)
 
 read_neg20_6546 <- read_excel("inst/extdata/batch_read_neg20.xlsx",
                               sheet = "20 Neg 6546") %>%
-  drop_na()
+  drop_na() %>%
+  mutate(COLLISIONENERGY = "20 eV")
 
 usethis::use_data(read_neg20_6546, overwrite = TRUE)
