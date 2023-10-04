5b04d25ec7ea6b84503aefd2463c3d95f240d1e5
Cristian Quiroz-Moreno <cristianquirozd1997@gmail.com>
2023-10-04 11:31:40 -0400

    Updating Neg 60 6546

--- data-raw/read_neg60_6546.R
+++ data-raw/read_neg60_6546.R
@@ -1,8 +1,10 @@
 ## code to prepare `read_neg60_6546` dataset goes here
 
-read_neg60_6546 <- read.csv("inst/extdata/csv/read_neg60_6546_excel.csv")
+read_neg60_6546 <- read_excel("inst/extdata/batch_read_neg20.xlsx",
+                              sheet = "60 neg 6546") %>%
+  drop_na() %>%
+  mutate(COLLISIONENERGY = "60 eV")
 
-read_neg60_6546 <- read_neg60_6546 %>% filter(!(File %in% "") )
 
 
 usethis::use_data(read_neg60_6546, overwrite = TRUE)
