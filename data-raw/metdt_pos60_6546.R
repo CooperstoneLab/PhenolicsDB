0e34e5d52ee48b836a7b92ba4f48a8faa4369bf7
Cristian Quiroz-Moreno <cristianquirozd1997@gmail.com>
2023-10-04 21:42:44 -0400

    Updating pos 60 80

--- data-raw/metdt_pos60_6546.R
+++ data-raw/metdt_pos60_6546.R
@@ -1,11 +1,10 @@
 ## code to prepare `metdt_pos60_6546` dataset goes here
 
-# Reading data
-data("read_pos6080_6546")
 
-#reading metadata
-lb_metadata <- read.csv("inst/extdata/csv/batch_msp_metadata.csv")  %>%
-  filter(!(SMILES %in% ""))
+lb_metadata <- read_xlsx("inst/extdata/batch_msp_metadata.xlsx", sheet = 1)
+
+load("data/read_pos6080_6546.rda")
+
 
 
 library_info <- dplyr::left_join(read_pos6080_6546, lb_metadata)
@@ -13,7 +12,7 @@ library_info <- dplyr::left_join(read_pos6080_6546, lb_metadata)
 metdt_pos60_6546 <- library_info %>%
   dplyr::mutate(PRECURSORTYPE = "[M+H]+",
                 COLLISIONENERGY = "60eV",
-                INSTRUMENTTYPE = "LC-ESI-QTOF",
+                INSTRUMENTTYPE = "ESI-QTOF",
                 IONMODE= "Positive") %>%
   dplyr::select(-Ionization_mode, -min_rt, -max_rt, -File) %>%
   dplyr::rename(NAME = Name, FORMULA = Formula)
